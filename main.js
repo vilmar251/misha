f068be8c14825ac15d685b9b4f91453d1198d508
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-22 13:03:29 +0300

    второй коммит

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-let num = 10;
-if (num % 3 === 0 && num % 5 === 0) {
-    console.log("FizzBuzz");
-} else if (num % 3 === 0) {
-    console.log("Fizz");
-} else if (num % 5 === 0) {
-    console.log("Buzz");
-}
+=
