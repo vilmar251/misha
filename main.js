d9a8943bd2a2b7e6bed0b721f4d0e434b4fb7218
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-29 16:43:58 +0300

    третье задание на цикл for

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-let height = 5;
+const matrix = [
+    ['Id', 'Title', 'Description'],
+    [1, 'Title-1', 'Description-1'],
+    [2, 'Title-2', 'Description-2'],
+    [3, 'Title-3', 'Description-3', 'Поле с багом'],
+    [4, 'Title-4', 'Description-4'],
+    [5, 'Title-5', 'Description-5'],
+    [6, 'Title-6', 'Description-6', 'Поле с багом'],
+];
 
-for (let i = 1; i <= height; i++) {
-    let stars = '*'.repeat(2 * i - 1);
-    let spaces = ' '.repeat(height - i);
-    console.log(spaces + stars);
+for (let i = 0; i < matrix.length; i++) {
+    let row = '';
+    for (let j = 0; j < matrix[i].length; j++) {
+        row += matrix[i][j] + '\t';
+    }
+    console.log(row);
 }
