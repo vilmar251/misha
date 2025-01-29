5cef24d8d96e21a491789bb968f2b42f1818f895
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-29 16:51:03 +0300

    четвертое задание на цикл for

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-const matrix = [
-    ['Id', 'Title', 'Description'],
-    [1, 'Title-1', 'Description-1'],
-    [2, 'Title-2', 'Description-2'],
-    [3, 'Title-3', 'Description-3', 'Поле с багом'],
-    [4, 'Title-4', 'Description-4'],
-    [5, 'Title-5', 'Description-5'],
-    [6, 'Title-6', 'Description-6', 'Поле с багом'],
-];
+const numbers = [1, 2, 3, 4, 5, -9007199254740991, 9007199254740991]; // пример массива чисел
 
-for (let i = 0; i < matrix.length; i++) {
-    let row = '';
-    for (let j = 0; j < matrix[i].length; j++) {
-        row += matrix[i][j] + '\t';
-    }
-    console.log(row);
-}
+let min = Math.min(...numbers); // находим минимальное значение
+let max = Math.max(...numbers); // находим максимальное значение
+
+console.log(`Минимальное: ${min}`);
+console.log(`Максимальное: ${max}`);
+
