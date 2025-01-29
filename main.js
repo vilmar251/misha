4d6fcbcd3848294cdf2c0396e68616cd4c49753f
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-29 16:19:56 +0300

    второе задание на цикл for

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let string = ["apple", "banana", "cherry", "date", "elderberry"];
-let half = Math.floor(string.length / 2);
-let secondHalf = string.slice(half);
+let height = 5;
 
-console.log(secondHalf);
+for (let i = 1; i <= height; i++) {
+    let stars = '*'.repeat(2 * i - 1);
+    let spaces = ' '.repeat(height - i);
+    console.log(spaces + stars);
+}
