5ced2f68f75b30aa93b4f4b545f9a52e37ebdce5
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-29 16:11:25 +0300

    Задание на цикл for

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-let attempts = 0;
-let diсe1=0
-let dice2=0
+let string = ["apple", "banana", "cherry", "date", "elderberry"];
+let half = Math.floor(string.length / 2);
+let secondHalf = string.slice(half);
 
-while (true) {
-    dice1 = Math.floor(Math.random() * 6) + 1;
-    dice2 = Math.floor(Math.random() * 6) + 1;
-    attempts++;
-    if (dice1 === dice2) break;
-}
-
-console.log(`Дубль выпал после ${attempts} попыток.`);
+console.log(secondHalf);
