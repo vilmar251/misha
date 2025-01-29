cbc1111f7921a1dc55d1524072cc7a6245879fc6
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-01-29 16:03:54 +0300

    задание на цикл while

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+let attempts = 0;
+let diсe1=0
+let dice2=0
+
+while (true) {
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2 = Math.floor(Math.random() * 6) + 1;
+    attempts++;
+    if (dice1 === dice2) break;
+}
+
+console.log(`Дубль выпал после ${attempts} попыток.`);
