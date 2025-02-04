d2ebc84a63a8d64c02f28e0b02ea75d24f6de94c
Михаил Лель <mihaillel@MacBook-Air-Mihail.local>
2025-02-04 11:32:50 +0300

    задание на яблоки

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-const person = { age: 10, name: '', child: 'Max' };
+const mary = {
+    name: 'Mary',
+    wantApples: 2,
+};
 
-for (let key in person) {
-    console.log(`${key}: ${person[key]}`);
+const alex = {
+    name: 'Alex',
+    wantApples: 1,
+};
+
+const mike = {
+    name: 'Mike',
+    wantApples: 5,
+};
+
+const brown = {
+    name: 'Brown',
+    wantApples: 4,
+};
+
+const people = [mary, alex, mike, brown];
+const apples = 11;
+
+const applesPerPerson = Math.floor(apples / people.length);
+const happy = [];
+const notHappy = [];
+
+for (const person of people) {
+    person.eaten = applesPerPerson;
+    person.isHappy = person.eaten >= person.wantApples;
+
+    if (person.isHappy) {
+        happy.push(person);
+    } else {
+        notHappy.push(person);
+    }
 }
+
+console.log('Довольные:', happy);
+console.log('Недовольные:', notHappy);
