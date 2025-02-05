e317495ad834a5a9ec9e3aa7af592f408b684001
vilmar251 <lelmisa566@gmail.com>
2025-02-05 16:35:51 +0300

    задание на цикл while

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-const object1 = { isAdult: true, email: 'example@mail.com', page: 100 };
-const object2 = { page: 100, isAdult: true, email: 'example@mail.com' };
-
-const keys1 = Object.keys(object1);
-const keys2 = Object.keys(object2);
-let areEqual = keys1.length === keys2.length;
-
-if (areEqual) {
-    for (const key of keys1) {
-        if (object1[key] !== object2[key]) {
-            areEqual = false;
-            break;
-        }
-    }
-}
-
-console.log(areEqual);
+console.log('Hello World!');
