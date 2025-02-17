f54054db9cfd4cf186335d432189a300ecc19cb2
vilmar251 <lelmisa566@gmail.com>
2025-02-17 13:54:38 +0300

    шифр

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
-import { faker } from '@faker-js/faker';
+const original = 'abcdefghijklmnopqrstuvwxyz';
+const cipher = 'oaklgybpwitedztsqgfchmjrnxv';
 
-function findStasAge(people: { name: string; age?: number | null }[]): string {
-  for (const person of people) {
-    if (person.name === 'Stas') {
-      if (person.age == null) return 'Возрастa нет';
-      return String(person.age);
-    }
-  }
-  return 'Объект не найден';
-}
-const testData = [
-  { name: 'Alex', age: 18 },
-  { name: 'Maria', age: 20 },
-  { name: 'Stas', age: faker.helpers.maybe(() => faker.number.int({ min: 18, max: 60 })) },
-  { name: 'Ivan', age: 10 },
-];
-console.log(findStasAge(testData));
+const decrypt = (text: string) => {
+  return text.replace(/[a-z]/g, (char) => original[cipher.indexOf(char)] || char);
+};
+const encryptedText = 'ntg ajuk fjbydv vikjo citvikh yd mkjidy dv qjujhp iyco. ptdvijoh!';
+console.log(decrypt(encryptedText));
