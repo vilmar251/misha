e01134abd1ca94269d43c6e3716ec5696e74eb4b
vilmar251 <lelmisa566@gmail.com>
2025-02-17 12:58:57 +0300

    наибольший делитель

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
-type User = { name?: string | null };
-const users: User[] = [{}, { name: null }, { name: '' }, { name: 'Алексей' }];
-
-const user1 = users[0].name ?? 'не обнаружено';
-const result1 = user1 || 'пусто';
-console.log(result1);
-
-const user2 = users[1].name ?? 'не обнаружено';
-const result2 = user2 || 'пусто';
-console.log(result2);
-
-const user3 = users[2].name ?? 'не обнаружено';
-const result3 = user3 || 'пусто';
-console.log(result3);
-
-const user4 = users[3].name ?? 'не обнаружено';
-const result4 = user4 || 'пусто';
-console.log(result4);
+function numb(num: number): number {
+  for (let i = Math.floor(num / 2); i >= 1; i--) {
+    if (num % i === 0) return i;
+  }
+  return 1;
+}
+const numbers = [1, 10, 11, 12, 21, 61, 85, 123];
+numbers.forEach((num) => console.log(`${num} - ${numb(num)}`));
