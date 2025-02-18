8109074f18cb3649c7e98709821733906c78d446
vilmar251 <lelmisa566@gmail.com>
2025-02-18 14:53:50 +0300

    каскадеры

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,74 @@
-function say(reName: string) {
-  return function (name: string): string {
-    return `${reName}, ${name}!`;
-  };
-}
+const alice = {
+  name: 'Alice',
+  age: 10,
+  illness: null,
+  driverLicenses: ['B'],
+};
 
-const sayHi = say('Hi');
-const sayBye = say('Bye');
+const bob = {
+  name: 'Bob',
+  age: 18,
+  illness: 'Cold',
+  driverLicenses: ['B'],
+};
 
-console.log(sayHi('Ruslan')); // Hi, Ruslan!
-console.log(sayHi('Maxim')); // Hi, Maxim!
+const charlie = {
+  name: 'Charlie',
+  age: 17,
+  illness: null,
+  driverLicenses: ['C', 'E'],
+};
 
-console.log(sayBye('Ruslan')); // Bye, Ruslan!
-console.log(sayBye('Maxim')); // Bye, Maxim!
+const diana = {
+  name: 'Diana',
+  age: 22,
+  illness: 'H. Disease',
+  driverLicenses: ['A', 'C', 'D'],
+};
+
+const eve = {
+  name: 'Eve',
+  age: 35,
+  illness: null,
+  driverLicenses: ['A', 'B', 'E'],
+};
+
+const frank = {
+  name: 'Frank',
+  age: 37,
+  illness: null,
+  driverLicenses: [],
+};
+
+const gorge = {
+  name: 'Gorge',
+  age: 28,
+  illness: null,
+  driverLicenses: ['B', 'D'],
+};
+
+const persons = [alice, bob, charlie, diana, eve, frank, gorge];
+
+const invalidPersons: any = [];
+const validPersons: any = [];
+persons.forEach((person) => {
+  if (person.age < 18) {
+    invalidPersons.push({ ...person, reason: 'Молодой' });
+    return;
+  }
+
+  if (person.illness) {
+    invalidPersons.push({ ...person, reason: 'Болен' });
+    return;
+  }
+
+  if (!person.driverLicenses.includes('B')) {
+    invalidPersons.push({ ...person, reason: 'Нет прав' });
+    return;
+  }
+
+  validPersons.push(person);
+});
+
+console.log('Для трюка подходят:', validPersons);
+console.log('\nДля трюка НЕ подходят:', invalidPersons);
