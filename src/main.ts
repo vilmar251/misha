7fcaba709e8b7bedc4ee8fb8cef27e989744a642
vilmar251 <lelmisa566@gmail.com>
2025-02-18 13:07:29 +0300

    фиббоначи

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-const original = 'abcdefghijklmnopqrstuvwxyz';
-const cipher = 'oaklgybpwitedztsqgfchmjrnxv';
+function fib(n: number): number {
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
 
-const decrypt = (text: string) => {
-  return text.replace(/[a-z]/g, (char) => original[cipher.indexOf(char)] || char);
-};
-const encryptedText = 'ntg ajuk fjbydv vikjo citvikh yd mkjidy dv qjujhp iyco. ptdvijoh!';
-console.log(decrypt(encryptedText));
-о;
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(9));
+console.log(fib(10));
+console.log(fib(11));
+console.log(fib(12));
