2b456c60feda42ace34a56a53c18e24f8c5c11d8
vilmar251 <lelmisa566@gmail.com>
2025-02-24 18:38:31 +0300

    Простой merge в новый объект

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,11 @@
-const alice = {
-  name: 'Alice',
-  age: 10,
-  illness: null,
-  driverLicenses: ['B'],
+const mergeToNewObject = (a: any, b: any) => {
+  return { ...a, ...b };
 };
 
-const bob = {
-  name: 'Bob',
-  age: 18,
-  illness: 'Cold',
-  driverLicenses: ['B'],
-};
+const obj1 = { a: 10, b: 100, c: 1 };
+const obj2 = { a: 20, b: 200, e: 2 };
+const obj3 = mergeToNewObject(obj1, obj2);
 
-const charlie = {
-  name: 'Charlie',
-  age: 17,
-  illness: null,
-  driverLicenses: ['C', 'E'],
-};
-
-const diana = {
-  name: 'Diana',
-  age: 22,
-  illness: 'H. Disease',
-  driverLicenses: ['A', 'C', 'D'],
-};
-
-const eve = {
-  name: 'Eve',
-  age: 35,
-  illness: null,
-  driverLicenses: ['A', 'B', 'E'],
-};
-
-const frank = {
-  name: 'Frank',
-  age: 37,
-  illness: null,
-  driverLicenses: [],
-};
-
-const gorge = {
-  name: 'Gorge',
-  age: 28,
-  illness: null,
-  driverLicenses: ['B', 'D'],
-};
-
-const persons = [alice, bob, charlie, diana, eve, frank, gorge];
-
-const invalidPersons: any = [];
-const validPersons: any = [];
-persons.forEach((person) => {
-  if (person.age < 18) {
-    invalidPersons.push({ ...person, reason: 'Молодой' });
-    return;
-  }
-
-  if (person.illness) {
-    invalidPersons.push({ ...person, reason: 'Болен' });
-    return;
-  }
-
-  if (!person.driverLicenses.includes('B')) {
-    invalidPersons.push({ ...person, reason: 'Нет прав' });
-    return;
-  }
-
-  validPersons.push(person);
-});
-
-console.log('Для трюка подходят:', validPersons);
-console.log('\nДля трюка НЕ подходят:', invalidPersons);
+console.log(obj1); // { a: 10, b: 100, c: 1 }
+console.log(obj2); // { a: 20, b: 200, e: 2 }
+console.log(obj3); // { a: 20, b: 200, c: 1, e: 2 }
