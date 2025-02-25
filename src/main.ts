3a2e51cb947ac45710902443234f9f1cea31e4d2
vilmar251 <lelmisa566@gmail.com>
2025-02-25 12:59:50 +0300

    Сортировка

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
-const storage = [
-  { age: 10, name: 'first' },
-  { age: 20, name: 'second' },
-  { age: 30, name: 'third' },
-  { age: 40, name: 'fourth' },
+type User = { id: number; age: number; name: string };
+
+const sort = (array: User[], key: keyof User, direction: 'asc' | 'desc' = 'asc'): User[] => {
+  return [...array].sort((a, b) => {
+    if (a[key] < b[key]) return direction === 'asc' ? -1 : 1;
+    if (a[key] > b[key]) return direction === 'asc' ? 1 : -1;
+    return 0;
+  });
+};
+
+const users: User[] = [
+  { id: 1, age: 10, name: 'zxc' },
+  { id: 3, age: 20, name: 'juk' },
+  { id: 6, age: 2, name: 'aa' },
+  { id: 9, age: 1, name: 'ab' },
+  { id: 2, age: 5, name: 'zz' },
 ];
 
-const storage2 = storage.map((item) => ({
-  age: item.name,
-  name: item.age,
-  reversed: true,
-}));
+const sortedByIdAsc = sort(users, 'id');
+console.log(sortedByIdAsc);
 
-console.log(storage2);
+const sortedByNameDesc = sort(users, 'name', 'desc');
+console.log(sortedByNameDesc);
+
+const sortedByAgeDesc = sort(users, 'age', 'desc');
+console.log(sortedByAgeDesc);
+
+console.log(users);
